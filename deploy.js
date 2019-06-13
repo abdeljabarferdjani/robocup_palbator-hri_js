a012225f22e7626dabf966472b909543e961cb64
Jonathan Guichard <jonathan-guichard@etu.univ-lyon1.fr>
2019-06-13 16:30:27 +0200

    Make deploy.js more generic
    Former-commit-id: 2e76b2d4da7ba05263a5f518131aa084c89e7b55

--- deploy.js
+++ deploy.js
@@ -2,36 +2,50 @@
 
 const os = require("os");
 const exec = require('child_process').exec;
+const process = require("process");
+const ArgumentParser = require("argparse").ArgumentParser;
 
-const user = "nao";
-const ip = "192.168.42.19";
-const appName = "R2019";
-const naoqiIsPresent = true;
+const parser = ArgumentParser({
+	version: 1,
+	addHelp: true,
+	description: "Tool to automaticaly deploy js to Pepper"
+});
+
+parser.addArgument(['--ip'], {help: "Ip of Pepper"});
+parser.addArgument(['--app'], {
+	help: "The Naoqi app's name",
+	defaultValue: "R2019"
+});
+parser.addArgument(['--user'], {help: "User for Pepper", defaultValue: "nao"});
+
+const args = parser.parseArgs();
+
+const {user, ip, app : appName} = args;
 
 const cwd = process.cwd();
 
 
-console.log("Deploying");
 
-if (naoqiIsPresent) {
-	
+if(ip) {
+	console.log("Deploying from", os.platform());
+	let command = "";
 	switch (os.platform()) {
 		case "linux":
-			exec(`echo  "put -r ${cwd}/build/* /home/nao/.local/share/PackageManager/apps/${appName}/html" | sftp ${user}@${ip} `, (err, stdout, strerr) => {
-				if (err) throw err;
-				console.log("Data pushed");
-			});
+			command = `echo  "put -r ${cwd}/build/* /home/nao/.local/share/PackageManager/apps/${appName}/html" | sftp ${user}@${ip}`
 			break;
 		case "win32":
-			exec(`echo  put -r ${cwd}/build/ /home/nao/.local/share/PackageManager/apps/${appName}/html | sftp ${user}@${ip} `, (err, stdout, strerr) => {
-				if (err) throw err;
-				console.error(strerr);
-				console.log("Data pushed");
-			});
+			command = `echo  put -r ${cwd}/build/ /home/nao/.local/share/PackageManager/apps/${appName}/html | sftp ${user}@${ip} `
 			break;
-		
-		
 		default:
 			break;
 	}
+	
+	exec(command, (err) => {
+		if (err) throw err;
+		console.log("Data pushed");
+	});
 }
+else {
+	console.error("No ip provided please use flag --ip or see --help")
+}
+
