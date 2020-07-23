6197f9daea2d9c56aebe82b7280bf1bddd779242
CPE Etudiant <thomas.cure@cpe.fr>
2020-07-23 09:49:30 +0200

    Updating LookForGuest view for Receptionist

--- src/view/Scenario/Receptionist/LookForGuest/index.jsx
+++ src/view/Scenario/Receptionist/LookForGuest/index.jsx
@@ -5,44 +5,94 @@ import './LookForGuest.css';
 import {comAction} from "../../../../redux/actions/CommunicationAction";
 import { viewAction } from '../../../../redux/actions/ViewAction';
 import ComponentTitle from "../../../Global/reusableComponent/ComponentTitle";
-
-function mapStateToProps(state) {
-	return {};
-}
+import {Doughnut} from 'react-chartjs-2'
 
 class LookForGuest extends Component {
 	
 	static propTypes = {
-		textToShow: PropTypes.string.isRequired,
+		time: PropTypes.number.isRequired
 	};
-
+	
+	constructor(props) {
+		super(props);
+		this.state = {
+			remainingTime: this.props.time,
+			elapsedTime: 0
+		}
+	}
 	
 	componentDidMount() {
-		this.props.viewOk();
-		//this.viewOk();
+		const self = this;
+		const intervalId = setInterval(() => {
+			self.setState(prev => {
+				return {
+					...prev,
+					remainingTime: prev.remainingTime - 1,
+					elapsedTime: prev.elapsedTime + 1
+				}
+			}, () => {
+				if (self.state.remainingTime === 0) {
+					clearInterval(intervalId);
+					this.props.viewOk();
+				}
+			})
+			
+			
+		}, 1000);
 	}
+	
 	
 	render() {
 		
+		let title = this.props.textToShow;
+		const data = {
+			labels: [
+				"", ""
+			],
+			datasets: [{
+				data: [this.props.time - this.state.elapsedTime, this.state.elapsedTime],
+				backgroundColor: [
+					'#12c4ff',
+					'rgba(54,162,235,0)',
+				],
+				
+			}]
+		};
+		
+		const options = {
+			elements: {
+				arc: {
+					borderWidth: 0
+				},
+			},
+			legend: {
+				display: false
+			},
+			tooltips: {
+				enabled: false
+			},
+			responsive: true,
+			scales: {xAxes: [{display: false,}], yAxes: [{display: false,}],},
+			
+			
+		};
+		const chart = <Doughnut data={data} options={options}/>;
+		
 		return (
-			<div className={"LookForGuest"}>
-				<ComponentTitle>{this.props.textToShow}</ComponentTitle>
+			<div id="LookForGuest">
+				<ComponentTitle>{title}</ComponentTitle>
+				<div id="chart">
+					<span className={"timer"}>{this.state.remainingTime}</span>
+					{chart}
+				</div>
 			</div>
 		);
 	}
-/*
-	viewOk() {
-		this.props.store.dispatch({
-			type: viewAction.getIndexCurrentAction.type
-		});
-		this.props.store.dispatch({
-			type: comAction.dataJs.type,
-			data: {status: 200}
-		})
-	}
-*/
-
 }
+
+const mapStateToProps = (state) => {
+	return {}
+};
 
 const mapDispatchToProps = (dispatch) => {
 	return {
@@ -57,5 +107,6 @@ const mapDispatchToProps = (dispatch) => {
 		}
 	}
 };
+
 
 export default connect(mapStateToProps, mapDispatchToProps)(LookForGuest);
